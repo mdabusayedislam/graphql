da5d81d5bbb2ded1ce0527acdcab796ca1272c3b
mdabusayedislam <abusayediit@gmail.com>
2021-10-14 13:21:38 +0600

    first commit

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const express=require('express')
+const { graphqlHTTP }=require('express-graphql');
+const schema=require('./schema/schema');
+const mongoose=require('mongoose');
+
+const app=express();
+mongoose.connect('mongodb+srv://sayed:MxEhTVEI6r715yMF@democluster.yosfh.mongodb.net/DemoDatabase?retryWrites=true&w=majority');
+mongoose.connection.once('open',()=>{
+console.log('connected to database');
+});
+
+app.use('/graphql',graphqlHTTP({
+schema,
+graphiql:true
+
+}));
+app.listen(4000,()=>{
+console.log('now listening for requests on port 4000');
+});
