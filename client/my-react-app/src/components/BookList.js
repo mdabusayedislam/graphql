e97df4c070774338d530b149a7c7ac19ebd98650
mdabusayedislam <abusayediit@gmail.com>
2021-10-16 00:04:19 +0600

    react-app

---
+++ client/my-react-app/src/components/BookList.js
@@ -0,0 +1,11 @@
+function BookList() {
+    return (
+      <div id="main">
+        <ul id="book-list">
+            <li>Book Name</li>
+        </ul>
+      </div>
+    );
+  }
+  export default BookList;
+  
