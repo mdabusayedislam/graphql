3eeb5575ea5fa2a857d50ee4f31c0d86d468f261
mdabusayedislam <abusayediit@gmail.com>
2021-10-23 00:47:33 +0600

    Book Component

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -1,9 +1,28 @@
-function App() {
+import React,{Component} from 'react'
+import BookList from './components/BookList';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+});
+
+class App extends Component {
+  render(){
   return (
+    <ApolloProvider client={client}>
     <div id="main">
-      <h1>Sayed Learning List</h1>      
+      <h1>Sayed Learning List 23</h1>  
+      <BookList/>  
     </div>
+    </ApolloProvider>
   );
 }
-
+}
 export default App;
